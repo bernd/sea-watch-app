f0c8c0407892a1a379d05a58ba4b6540cf48368f
Niczem <nic@transparency-everywhere.com>
2016-01-30 04:32:56 +0100

    routes updated

--- admin/app/Http/routes.php
+++ admin/app/Http/routes.php
@@ -35,40 +35,108 @@ Route::post('operation_areas/create', 'operation_areas@store');
 // route to show our edit form
 Route::get('cases/edit/{id}', array('as' => 'case.edit', function($id) 
 {
-    // return our view and Nerd information
-    return View::make('cases.update') // pulls app/views/nerd-edit.blade.php
+    return View::make('cases.update')
         ->with('case', emergencyCase::find($id));
 }));
 
 Route::post('cases/edit/{id}', 'EmergencyCaseController@update');
 
+/**
+ * @api {post} /api/messages/send SendMessage
+ * @apiDescription sends message and adds location
+ * @apiName SendMessage
+ * @apiGroup message
+ *
+ * @apiParam {Number} emergency_case_id id of the opened case.
+ * @apiParam {String} sender_type refugee/land_operator_rumors.
+ * @apiParam {String} sender_id sender device unique id/fingerprint
+ * @apiParam {String} geo_data geo JSON string
+ *
+ * @apiSuccess {String} firstname Firstname of the User.
+ * @apiSuccess {String} lastname  Lastname of the User.
+ */
 Route::post('api/messages/send', 'ApiController@sendMessage');
 
+
+/**
+ * @api {get} /api/cases/create CreateCase
+ * @apiDescription creates new case if submitted geolocation is in any operation area
+ * @apiName CreateCase
+ * @apiGroup case
+ * 
+ * @apiParam {String} [status] 
+ * @apiParam {String} [condition] 
+ * @apiParam {String} [boat_type] 
+ * @apiParam {bool} [other_involved] 
+ * @apiParam {bool} [engine_working] 
+ * @apiParam {Number} [passenger_count] 
+ * @apiParam {String} [additional_informations] 
+ * @apiParam {Number} [spotting_distance] 
+ * @apiParam {Number} [spotting_direction] in degrees
+ * @apiParam {String} [picture] base64 encoded picture
+ * @apiParam {String} source source
+ * @apiParam {String} location_data  geo JSON string
+ *
+ * @apiSuccess {Number} case_id
+ */
 Route::post('api/cases/create', 'ApiController@add_request');
 
+/**
+ * @api {post} api/cases/sendMessageCrew submit message (only for backend, auth required)
+ * @apiName SendMessageCrew
+ * @apiGroup message
 
-//not supposed for usage with the app!
+ * @apiParam {Number} case_id
+ * @apiParam {String} message
+ *
+ * @apiSuccess {Number} message_id
+ */
 Route::post('api/cases/sendMessageCrew', 'ApiController@sendMessageCrew');
 
+/**
+ * @api {get} api/cases/operation_area/:id CasesInOperationArea
+ * @apiDescription sends get cases in operation area (for backend)
+ * @apiName CasesInOperationArea
+ * @apiGroup cases
+ *
+ * @apiSuccess {String} JSON cases
+ */
 Route::get('api/cases/operation_area/{id}', 'ApiController@casesInOperationArea');
 
-Route::post('api/cases/ping', 'ApiController@ping');
-
+/**
+ * @api {post} api/cases/getInvolved GetInvolved
+ * @apiDescription adds user to db table involved_users and returns message (auth required)
+ * @apiName GetInvolved
+ * @apiGroup cases
+ * @apiParam {Number} case_id
+ *
+ * @apiSuccess {String} JSON emergency_case_messages
+ */
 Route::post('api/cases/getInvolved', 'ApiController@getInvolved');
 
+
+/**
+ * @api {post} api/reloadApp reloadApp
+ * @apiDescription updates location and receives new messages
+ * @apiName ReloadApp
+ * @apiParam {Number} emergency_case_id
+ * @apiParam {Number} last_message_received
+ * @apiParam {String} geo_data geo JSON string
+ *
+ * @apiSuccess {String} JSON messages
+ */
 Route::post('api/reloadApp', 'ApiController@reloadApp');
 
+/**
+api/reloadBackend reload backend (auth required)
+ */
 Route::post('api/reloadBackend', 'ApiController@reloadBackend');
 
+
+
+//Deprecated
 Route::post('api/checkForUpdates', 'ApiController@checkForUpdates');
-
-
-
-
-//Route::any('aapi/cases/create', function(){
-//    return 'asdasd';
-//});
-
+Route::post('api/cases/ping', 'ApiController@ping');
 
 
 
